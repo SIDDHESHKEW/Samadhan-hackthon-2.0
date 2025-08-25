21471750dc8952c44db30f70606722457f65e465
Vaishnavi <vaishnavihulke13@gmail.com>
2025-08-25 18:28:49 +0530

    Day1
    Print student details from an object

---
+++ student.js
@@ -0,0 +1,16 @@
+// Create a student object
+const student = {
+    name: "Siddhesh kewate",
+    age: 19,
+    course: "AI/ML",
+    rollNo: 85,
+    college: "Scope Global Skills University"
+};
+
+// Print student details
+console.log("Student Details:");
+console.log("Name: " + student.name);
+console.log("Age: " + student.age);
+console.log("Course: " + student.course);
+console.log("Roll No: " + student.rollNo);
+console.log("College: " + student.college);
